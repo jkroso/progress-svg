eed67d53236a157a49b7a6cef9db0ea3a352a5a3
Jake Rosoman <jkroso@gmail.com>
2013-12-14 21:59:45 +1300

    position:auto

--- template.js
+++ template.js
@@ -1,1 +1,1 @@
-module.exports = '<svg xmlns="http://www.w3.org/2000/svg" link="http://www.w3.org/1999/xlink" style="position:relative;width:100%;height:100%;" version="1.1"><defs></defs><path></path><text>0</text></svg>'
+module.exports = '<svg xmlns="http://www.w3.org/2000/svg" link="http://www.w3.org/1999/xlink" style="width:100%;height:100%;" version="1.1"><defs></defs><path></path><text>0</text></svg>'
